8d0cc6c39d035e3bcc9a821098b00c7876ad52eb
jaimedavid01 <jaimedavid01@yahoo.com>
2020-12-10 13:45:41 -0500

    Solved

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  # def show
+  # end
+
+  # def set_student
+  #   @student = Student.find(params[:id])
+  # end
+
 end
